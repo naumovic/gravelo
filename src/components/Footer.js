a85a05374180cfa3f9a4c820de998eeb5c9de0ad
Mihajlo Naumovic <mihajlo.naumovic@gmail.com>
2023-01-15 00:24:04 +1000

    Update Footer.js
    Footer updated with own info.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://www.instagram.com/thegravelo/">@thegravelo</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://thegravelo.com.au/">The Gravelo</a>.
         </span>
       </div>
     </footer>
